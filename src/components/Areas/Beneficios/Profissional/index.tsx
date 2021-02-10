10395a8f2e77a7970ef7a851bc4830982f683a5b
andrefigueira93 <andrefigueira93@gmail.com>
2021-02-09 21:51:21 -0300

    Ajustes Tecnicos nos Beneficios

--- src/components/Areas/Beneficios/Profissional/index.tsx
+++ src/components/Areas/Beneficios/Profissional/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BeneficioDTO from '../../../../dtos/beneficioDTO';
 import CheckSimples from '../../../Icones/CheckSimples';
 
-const BeneficioPessoal: React.FC = () => {
+const BeneficioProfissional: React.FC = () => {
   const beneficios: BeneficioDTO[] = [
     {
       name: 'Serviço 01',
@@ -48,17 +48,19 @@ const BeneficioPessoal: React.FC = () => {
 
   return (
     <>
-      <div id="beneficios" className="bg-white">
-        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto pb-16 px-4 sm:px-6 lg:pb-24 lg:px-8">
           <div className="max-w-3xl mx-auto text-center">
             <h2 className="leading-6 text-blue-600 font-semibold tracking-wide uppercase">
               Benefícios
             </h2>
-            <h3 className="text-3xl font-extrabold text-gray-900">Pessoais</h3>
+            <h3 className="text-3xl font-extrabold text-gray-900">
+              Profissionais
+            </h3>
             <p className="mt-4 text-lg text-gray-500">
               O processo de coaching traz inúmeros benefícios aos coachees,
               perceptíveis na vida pessoal e profissional. Separamos alguns dos
-              benefícios pessoais do coaching. Confira:
+              benefícios Profissionais do coaching. Confira:
             </p>
           </div>
           <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
@@ -80,4 +82,4 @@ const BeneficioPessoal: React.FC = () => {
   );
 };
 
-export default BeneficioPessoal;
+export default BeneficioProfissional;
---
+++ src/dtos/beneficioDTO.ts
@@ -0,0 +1,4 @@
+export default interface BeneficioDTO {
+  name: string;
+  text: string;
+}
